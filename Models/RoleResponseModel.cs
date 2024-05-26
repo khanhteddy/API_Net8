650a2ac9a40a76abebf4163ea3ceaefd0df26ea6
Khanh <khanhnc.nck@gmail.com>
2024-05-26 22:06:20 +0700

    add Models Auth, Role

---
+++ Models/RoleResponseModel.cs
@@ -0,0 +1,9 @@
+namespace Net8Angular17.Models
+{
+    public class RoleResponseModel
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public int TotalUsers { get; set; }
+    }
+}
